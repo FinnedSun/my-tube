b3081dbdd3ea9902a9d703de5310be5ba4ed51e3
FinnedSun <rayhanagungmaulana.04@gmail.com>
2025-02-20 06:36:33 +0700

    remove skeleton auth-button.tsx

--- src/modules/auth/ui/components/auth-button.tsx
+++ src/modules/auth/ui/components/auth-button.tsx
@@ -7,44 +7,37 @@ import {
   SignInButton,
   SignedIn,
   SignedOut,
-  ClerkLoaded,
-  ClerkLoading,
 } from "@clerk/nextjs"
-import { Skeleton } from '@/components/ui/skeleton'
+
 
 export const AuthButton = () => {
   // TODO: Add diffrent auth states
   return (
     <>
-      <ClerkLoading>
-        <Skeleton className="w-[30px] h-[30px] rounded-full" />
-      </ClerkLoading>
-      <ClerkLoaded>
-        <SignedIn>
-          <UserButton >
-            <UserButton.MenuItems>
-              {/* TODO: add user profile menu button */}
-              <UserButton.Link
-                href='/studio'
-                label='Studio'
-                labelIcon={<ClapperboardIcon className='size-4' />}
-              />
-              <UserButton.Action label='manageAccount' />
-            </UserButton.MenuItems>
-          </UserButton>
-        </SignedIn>
-        <SignedOut>
-          <SignInButton mode='modal'>
-            <Button
-              variant="outline"
-              className='px-4 py-2 text-sm font-medium text-blue-600 hover:text-blue-500 border-blue-500/20 rounded-full shdow-none'
-            >
-              <UserCircleIcon />
-              Sign in
-            </Button>
-          </SignInButton>
-        </SignedOut>
-      </ClerkLoaded>
+      <SignedIn>
+        <UserButton >
+          <UserButton.MenuItems>
+            {/* TODO: add user profile menu button */}
+            <UserButton.Link
+              href='/studio'
+              label='Studio'
+              labelIcon={<ClapperboardIcon className='size-4' />}
+            />
+            <UserButton.Action label='manageAccount' />
+          </UserButton.MenuItems>
+        </UserButton>
+      </SignedIn>
+      <SignedOut>
+        <SignInButton mode='modal'>
+          <Button
+            variant="outline"
+            className='px-4 py-2 text-sm font-medium text-blue-600 hover:text-blue-500 border-blue-500/20 rounded-full shdow-none'
+          >
+            <UserCircleIcon />
+            Sign in
+          </Button>
+        </SignInButton>
+      </SignedOut>
     </>
   )
 }
