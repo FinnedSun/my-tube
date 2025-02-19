94c6c4e72eca7497e4112406a39bd53a0ccac9ac
FinnedSun <rayhanagungmaulana.04@gmail.com>
2025-02-19 21:07:41 +0700

    page run test

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,19 +1,7 @@
 
 
-import { HydrateClient, trpc } from "@/trpc/server";
-import { PageClient } from "./client";
-import { Suspense } from "react";
-import { ErrorBoundary } from "react-error-boundary";
-
 export default async function Home() {
-  void trpc.hello.prefetch({ text: "John" })
   return (
-    <HydrateClient>
-      <Suspense fallback={<p>loading...</p>}>
-        <ErrorBoundary fallback={<p>error..</p>}>
-          <PageClient />
-        </ErrorBoundary>
-      </Suspense>
-    </HydrateClient>
+    <div>test</div>
   );
 }
