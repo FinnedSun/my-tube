addd92d99d13019b0eb606c3b57d7de3e3191b78
FinnedSun <rayhanagungmaulana.04@gmail.com>
2025-02-21 04:20:11 +0700

    fix error

--- src/app/(studio)/studio/page.tsx
+++ src/app/(studio)/studio/page.tsx
@@ -1,15 +1,18 @@
-import { DEFAULT_LIMIT } from "@/constants"
-import { StudioView } from "@/modules/studio/ui/view/studio-view"
-import { HydrateClient, trpc } from "@/trpc/server"
+// import { DEFAULT_LIMIT } from "@/constants"
+// import { StudioView } from "@/modules/studio/ui/view/studio-view"
+import { HydrateClient } from "@/trpc/server"
 
 
 const StudioPage = async () => {
-  void trpc.studio.getMany.prefetchInfinite({
-    limit: DEFAULT_LIMIT,
-  })
+  // void trpc.studio.getMany.prefetchInfinite({
+  //   limit: DEFAULT_LIMIT,
+  // })
   return (
     <HydrateClient>
-      <StudioView />
+      {/* <StudioView /> */}
+      <div>
+        <h1>Studio</h1>
+      </div>
     </HydrateClient>
   )
 }
