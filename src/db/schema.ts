3b44368852a63d8cd22fd90dc72e48cf7f43ee51
FinnedSun <rayhanagungmaulana.04@gmail.com>
2025-02-25 03:35:02 +0700

    video reactions

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -28,7 +28,8 @@ export const users = pgTable("users", {
 
 export const userRelations = relations(users, ({ many }) => ({
   videos: many(videos),
-  videoViews: many(videoViews)
+  videoViews: many(videoViews),
+  videoReactions: many(videoReactions)
 }))
 
 export const categories = pgTable("categories", {
@@ -87,16 +88,17 @@ export const videoRelations = relations(videos, ({ one, many }) => ({
     fields: [videos.categoryId],
     references: [categories.id],
   }),
-  views: many(videoViews)
+  views: many(videoViews),
+  reactions: many(videoReactions),
 }))
 
 export const videoViews = pgTable("video_views", {
   userId: uuid("user_id").references(() => users.id, {
     onDelete: "cascade",
-  }).notNull().notNull(),
+  }).notNull(),
   videoId: uuid("video_id").references(() => videos.id, {
     onDelete: "cascade",
-  }).notNull().notNull(),
+  }).notNull(),
   createdAt: timestamp("created_at").defaultNow().notNull(),
   updatedAt: timestamp("updated_at").defaultNow().notNull(),
 }, (t) => [
@@ -120,3 +122,40 @@ export const videoViewsRelations = relations(videoViews, ({ one }) => ({
 export const videoViewsSelectSchema = createSelectSchema(videoViews)
 export const videoViewsInsertSchema = createInsertSchema(videoViews)
 export const videoViewsUpdateSchema = createUpdateSchema(videoViews)
+
+export const reactionType = pgEnum("reaction_type", [
+  "like",
+  "dislike",
+])
+
+export const videoReactions = pgTable("video_reactions", {
+  userId: uuid("user_id").references(() => users.id, {
+    onDelete: "cascade",
+  }).notNull(),
+  videoId: uuid("video_id").references(() => videos.id, {
+    onDelete: "cascade",
+  }).notNull(),
+  type: reactionType("type").notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
+}, (t) => [
+  primaryKey({
+    name: "video_reactions_pk",
+    columns: [t.userId, t.videoId],
+  })
+])
+
+export const videoReactionRelations = relations(videoReactions, ({ one }) => ({
+  user: one(users, {
+    fields: [videoReactions.userId],
+    references: [users.id],
+  }),
+  video: one(videos, {
+    fields: [videoReactions.videoId],
+    references: [videos.id],
+  })
+}))
+
+export const videoReactionSelectSchema = createSelectSchema(videoReactions)
+export const videoReactionInsertSchema = createInsertSchema(videoReactions)
+export const videoReactionUpdateSchema = createUpdateSchema(videoReactions)
