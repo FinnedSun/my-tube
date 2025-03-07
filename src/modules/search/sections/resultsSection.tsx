45692fe33f48f88abe1a00e9860921e9d1886ec1
FinnedSun <rayhanagungmaulana.04@gmail.com>
2025-03-08 06:52:09 +0700

    imporivements

--- src/modules/search/sections/resultsSection.tsx
+++ src/modules/search/sections/resultsSection.tsx
@@ -3,9 +3,11 @@
 import { useIsMobile } from "@/hooks/use-mobile"
 import { trpc } from "@/trpc/client"
 
-import { VideoRowCard } from "@/modules/videos/ui/component/video-row-card"
-import { VideoGridCard } from "@/modules/videos/ui/component/video-grid-card"
+import { VideoRowCard, VideoRowCardSkeleton } from "@/modules/videos/ui/component/video-row-card"
+import { VideoGridCard, VideoGridCardSkeletion } from "@/modules/videos/ui/component/video-grid-card"
 import { InfiniteScroll } from "@/components/infinite-scroll"
+import { Suspense } from "react"
+import { ErrorBoundary } from "react-error-boundary"
 
 interface ResultsSectionProps {
   query: string | undefined
@@ -13,6 +15,42 @@ interface ResultsSectionProps {
 }
 
 export const ResultsSection = ({
+  query,
+  categoryId,
+}: ResultsSectionProps) => {
+  return (
+    <Suspense
+      key={`${query}-${categoryId}`}
+      fallback={<ResultsSectionSkeleton />}
+    >
+      <ErrorBoundary fallback={<p>Error...</p>}>
+        <ResultsSectionSuspense
+          query={query}
+          categoryId={categoryId}
+        />
+      </ErrorBoundary>
+    </Suspense>
+  )
+}
+
+const ResultsSectionSkeleton = () => {
+  return (
+    <div>
+      <div className="hidden flex-col gap-4 md:flex">
+        {Array.from({ length: 5 }).map((_, i) => (
+          <VideoRowCardSkeleton key={i} />
+        ))}
+      </div>
+      <div className="flex flex-col gap-4 p-4 gap-y-10 pt-6 md:hidden">
+        {Array.from({ length: 5 }).map((_, i) => (
+          <VideoGridCardSkeletion key={i} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+const ResultsSectionSuspense = ({
   query,
   categoryId,
 }: ResultsSectionProps) => {
